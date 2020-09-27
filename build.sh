a61a998f913c5352d01e09ec594edc26ff32c02e
Bukandewa <20709446+redstarksten@users.noreply.github.com>
2020-09-27 16:12:22 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo -e "Script by :\n"
 echo -e "#####  #    # #    #   ##   ##   # ####   #######     #   ## "
 echo -e "#    # #    # #  ##    ##   ###  # #   ## #     #  #  #   ## "
@@ -175,6 +175,6 @@ checker
 compile
 zipping
 signer
-gdrive
+#gdrive
 echo -e "Completed $ZIPNAME-signed.zip in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 

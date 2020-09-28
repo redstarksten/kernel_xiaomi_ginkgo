6db80eb3206862277137977599fd43b4489d8862
bukandewa <mahadewanto2@gmail.com>
2020-09-28 21:26:44 +0700

    tes build

--- circleci_build.sh
+++ circleci_build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+DTB=$(pwd)/out/arch/arm64/boot/dts/qcom/*.dtb
 echo "Clone Anykernel and GCC"
 apt-get update -y && apt-get upgrade -y
 apt-get install -y python3 git cmake clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python-clang build-essential make bzip2 libncurses5-dev lld libssl-dev python3-pip ninja-build
 git clone -j32 https://github.com/redstarksten/AnyKernel AnyKernel
+mkdir signer \
+curl -sLo signer/zipsigner-3.0.jar https://raw.githubusercontent.com/najahiiii/Noob-Script/noob/bin/zipsigner-3.0.jar
 git clone -j32 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 toolchain
 git clone -j32 https://github.com/NusantaraDevs/clang clang
 echo "Done"
@@ -12,8 +15,12 @@ chat_id="513350521"
 GCC="$(pwd)/gcc/bin/aarch64-linux-gnu-"
 tanggal=$(TZ=Asia/Jakarta date "+%Y%m%d-%H%M")
 START=$(date +"%s")
-export LD_LIBRARY_PATH="/root/clang/bin/../lib:$PATH"xport ARCH=arm64
-export KBUILD_BUILD_USER=bukandewa
+KERNEL="StarkX"
+DEVICE="Ginkgo"
+ZIPNAME="$KERNEL-$DEVICE-${tanggal}"
+export LD_LIBRARY_PATH="/root/clang/bin/../lib:$PATH"
+export ARCH=arm64
+export KBUILD_BUILD_USER=Bukandewa
 export KBUILD_BUILD_HOST=Circleci
 # sticker plox
 function sticker() {
@@ -32,12 +39,12 @@ function sendinfo() {
                         -d chat_id=$chat_id \
                         -d "disable_web_page_preview=true" \
                         -d "parse_mode=html" \
-                        -d text="<b>StarkX Kernel</b> New Build is UP!%0A<b>Started on :</b> <code>circleCI</code>%0A<b>For device :</b> <b>Ginkgo</b> (Redmi Note 8)%0A<b>Kernel Version :</b> <code>$(make kernelversion)</code>%0A<b>Branch :</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>%0A<b>Under commit :</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0A<b>Using compiler :</b> <code>$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0A<b>Started on :</b> <code>$(TZ=Asia/Jakarta date)</code>%0A<b>circleCI Status :</b> <a href='https://app.circleci.com/pipelines/github/redstarksten/kernel_xiaomi_ginkgo'>here</a>"
+                        -d text="<b>StarkX Kernel</b> New Update is Coming!%0A<b>Started on :</b> <code>circleCI</code>%0A<b>For device :</b> <b>Ginkgo</b> (Redmi Note 8)%0A<b>Kernel Version :</b> <code>$(make kernelversion)</code>%0A<b>Branch :</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>%0A<b>Under commit :</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0A<b>Using compiler :</b> <code>$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0A<b>Started on :</b> <code>$(TZ=Asia/Jakarta date)</code>%0A<b>circleCI Status :</b> <a href='https://app.circleci.com/pipelines/github/redstarksten/kernel_xiaomi_ginkgo'>here</a>"
 }
 # Push kernel to channel
 function push() {
         cd AnyKernel
-	ZIP=$(echo Mina-Kernel-*.zip)
+	ZIP=$(echo StarkX-Kernel-*.zip)
 	curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
 			-F chat_id="$chat_id" \
 			-F "disable_web_page_preview=true" \
@@ -83,17 +90,28 @@ make -j$(nproc --all) O=out \
 		stikerr
                 exit 1
             fi
-        cp out/arch/arm64/boot/Image.gz-dtb AnyKernel/Image.gz-dtb
+        cp $IMAGE AnyKernel/Image.gz-dtb
 }
 # Zipping
 function zipping() {
         cd AnyKernel
-        zip -r9 StarkX-Ginkgo-${tanggal}.zip *
+        cat $IMAGE $DTB > Anykernel/Image.gz-dtb
+        zip -r9 unsigned.zip *
+        mv unsigned.zip ../signer/
         cd ..
+}
+# Signing
+function signer() {
+    if [[ -f "$KERNEL_DIR/signer/unsigned.zip" ]]; then
+        cd signer
+        java -jar zipsigner-3.0.jar \
+        unsigned.zip $ZIPNAME-signed.zip
+    fi
 }
 sendinfo
 compile
 zipping
+signer
 END=$(date +"%s")
 DIFF=$(($END - $START))
 paste
